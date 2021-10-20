0a86360d14bbba590798f0aa86b01b3279c643d5
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-20 12:23:46 +0530

    Displaying Welcome Message on the Master Branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program//Address Book Main Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");//START Case for displaying the welcome message on the Master Branch.
+        }
+    }
+}
